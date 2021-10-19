750a5c0e78c2c03944e8448cc72a5b5874771531
Guilherme <guilhermegso1019@gmail.com>
2021-10-19 00:19:57 -0300

    create ssl configuration to production enviroment

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: 'default',
   type: 'postgres',
-  [process.env.TS_NODE_ENV === 'prod' ? ssl : undefined]: { rejectUnauthorized: false },
   url: process.env.DATABASE_URL,
   entities: [
     `./${process.env.TS_NODE_ENV === 'prod' ? 'dist' : 'src'}/modules/**/infra/typeorm/entities/*.{js,ts}`
@@ -11,5 +10,6 @@ module.exports = {
   ],
   cli: {
     migrationsDir: './src/infra/typeorm/migrations'
-  }
+  },
+  ...(process.env.TS_NODE_ENV === 'prod' && { ssl: { rejectUnauthorized: false } }),
 }
